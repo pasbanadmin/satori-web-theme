1821cf02f8feca11fbeed124c3a494bce9d921cb
suraj235 <er.surajsanwal@gmail.com>
2026-09-14 15:46:05 +0530

    feat: google analytics script added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,25 @@
 <!doctype html>
 <html @php(language_attributes())>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @php(do_action('get_header'))
-    @php(wp_head())
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @php(do_action('get_header'))
+  @php(wp_head())
 
-    <script type="application/ld+json">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-LMKN2TGELE"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-LMKN2TGELE');
+  </script>
+  <!-- End Google tag (gtag.js) -->
+  <script type="application/ld+json">
     {
       "@@context": "https://schema.org",
       "@@type": "Resort",
@@ -26,33 +37,34 @@
       }
     }
     </script>
-  </head>
+</head>
 
-  <body @php(body_class())>
-    @php(wp_body_open())
+<body @php(body_class())>
+  @php(wp_body_open())
 
-    <div id="app">
-      <a class="sr-only focus:not-sr-only" href="#main">
-        {{ __('Skip to content', 'sage') }}
-      </a>
+  <div id="app">
+    <a class="sr-only focus:not-sr-only" href="#main">
+      {{ __('Skip to content', 'sage') }}
+    </a>
 
-      @include('sections.header')
+    @include('sections.header')
 
-      <main id="main" class="main">
-        @yield('content')
-      </main>
+    <main id="main" class="main">
+      @yield('content')
+    </main>
 
-      @hasSection('sidebar')
-        <aside class="sidebar">
-          @yield('sidebar')
-        </aside>
-      @endif
+    @hasSection('sidebar')
+      <aside class="sidebar">
+        @yield('sidebar')
+      </aside>
+    @endif
 
-      @include('sections.footer')
-      @include('partials.whatsapp-widget')
-    </div>
+    @include('sections.footer')
+    @include('partials.whatsapp-widget')
+  </div>
 
-    @php(do_action('get_footer'))
-    @php(wp_footer())
-  </body>
+  @php(do_action('get_footer'))
+  @php(wp_footer())
+</body>
+
 </html>
